3b2f92d069ff1a6bd9d5ecfef5a38e9dac38d6bd
SrhTurjo <contactsrhhere@gmail.com>
2024-03-20 14:56:42 +0600

    cleared junk from weather

--- weather.py
+++ weather.py
@@ -1,46 +1,4 @@
 import requests, os, datetime
-from time import sleep
-
-
-api = os.environ.get('open_weather_api')
-
-
-def search_for_locations(text = "Type location: "):
-    search = input(text) 
-    g_link = f"http://api.openweathermap.org/geo/1.0/direct?q={search}&limit=5&appid={api}"
-    r = requests.get(url=g_link)
-    return list(r.json())
-  
-
-def get_coordinates():
-
-    results = search_for_locations()
-    if results == []:
-        print("Can't find the location. Please try naming the location differently.")
-        get_coordinates()
-    else:
-        for i,result in enumerate(results):
-            details = ['name', 'state', 'country']
-            print(f"\n{i+1}) ", end="")
-            for detail in details:
-                if detail in result:
-                    print(f"{result[detail]};", end=" ")
-        
-        
-        i = int(input(f"\nGive the index of your choice: "))-1
-        coordinates = (int(results[i]['lat']), int(results[i]['lon']))
-        return coordinates
-    
-def get_weather(unit="metric"):
-    lat, lon = get_coordinates()
-
-    w_link = f"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api}&units={unit}"
-    r = requests.get(url=w_link)
-    weather_data = list(dict(r.json())['list'])
-
-    for data in weather_data:
-        print(f"Time: {data['dt_txt']}, Temp: {data['main']['temp']}, Weather: {data['weather'][0]['description']}")
-
 
 class WeatherApp:
     def __init__(self):
@@ -121,5 +79,3 @@ class WeatherApp:
         self.today, self.tomorrow = today, tomorrow
         self.event_flag.set()
 
-if __name__ == "__main__":
-    get_weather()
